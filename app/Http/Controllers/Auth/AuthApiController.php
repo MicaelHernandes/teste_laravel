cdf4ffac0c92c2a30168bdc72acbee151ecc1426
micael <micael.hernandes@hnndevs.com>
2024-03-04 20:49:25 -0300

    feat: dependency injection in auth user controller

--- app/Http/Controllers/Auth/AuthApiController.php
+++ app/Http/Controllers/Auth/AuthApiController.php
@@ -3,9 +3,15 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\services\UserService;
+use App\services\UserServiceInterface;
 use Illuminate\Http\Request;
 
 class AuthApiController extends Controller
 {
-    //
+    private UserServiceInterface $services;
+    public function __construct(private UserService $service)
+    {
+        $this->services = $service;
+    }
 }
