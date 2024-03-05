4d1fd2768e8f2db92f34574c23e6c6eb02082807
Micael Hernandes <micael.hernandes@hnddevs.com.br>
2024-03-05 00:31:10 +0000

    feat : creating login method in user auth controller

--- app/Http/Controllers/Auth/AuthApiController.php
+++ app/Http/Controllers/Auth/AuthApiController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\UserStoreRequest;
 use App\services\UserService;
 use App\services\UserServiceInterface;
 use Illuminate\Http\Request;
@@ -13,5 +14,20 @@ class AuthApiController extends Controller
     public function __construct(private UserService $service)
     {
         $this->services = $service;
+    }
+
+    public function register(UserStoreRequest $request)
+    {
+        try {
+            $user = $this->services->registerUser($request->toArray());
+            return response()->json([
+                "message" => "User created successfully",
+                'user' => $user
+            ], 201);
+        } catch (\Throwable $e) {
+            return response()->json([
+                'message' => $e->getMessage()
+            ], 400);
+        }
     }
 }
